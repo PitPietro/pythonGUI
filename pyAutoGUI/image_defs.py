060754a71e922fea8bf2db1dcdff5c24a056f730
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-28 12:11:07 +0200

    Find on screen

--- pyAutoGUI/image_defs.py
+++ pyAutoGUI/image_defs.py
@@ -14,5 +14,21 @@ def make_screenshot(name=str(datetime.datetime.now()), ext='.png', path=os.getcw
     return gui.screenshot(file_path)
 
 
+def find_on_screen(file):
+    """
+    :param file: image
+    :return: the x and y coordinate and width and height dimensions of the image on the screen
+    """
+    return gui.locateOnScreen(file)
+
+
+def find_on_screen_center(file):
+    """
+    :param file: image
+    :return: the x and y coordinate of the center of the image on the screen
+    """
+    return gui.locateCenterOnScreen(file)
+
+
 if __name__ == '__main__':
     print(make_screenshot())
